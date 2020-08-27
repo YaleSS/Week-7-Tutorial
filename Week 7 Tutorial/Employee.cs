9dbead52cece70ec5cd9f828ec5f312c26552150
Yu Zhang <zhangy51@utas.edu.au>
2020-08-28 01:33:45 +1000

    Add project files.

---
+++ Week 7 Tutorial/Employee.cs
@@ -0,0 +1,20 @@
+﻿using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_7_Tutorial
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public Gender gender;
+
+        public override string ToString()
+        {
+            string s = "Name: " + name + ", ID: " + id + ", Gender: " + gender;
+            return s;
+        }
+    }
+}
---
+++ Week 7 Tutorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Week_7_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee() { name = "Jane", id = 1, gender = Gender.Female },
+                new Employee() { name = "James", id = 2, gender = Gender.Male },
+                new Employee() { name = "Kim", id = 3, gender = Gender.Male },
+                new Employee() { name = "Kitty", id = 4, gender = Gender.Female }
+            };
+            DisplayEmployeeList(employees);
+            DisplayEmployeeList(FilterByGender(employees, Gender.Male));
+        }
+
+        static void DisplayEmployeeList(List<Employee> employees)
+        {
+            foreach (Employee e0 in employees)
+            {
+                Console.WriteLine(e0.ToString()); 
+            }
+            Console.WriteLine("the end.");
+        }
+
+        static List<Employee> FilterByGender(List<Employee> staff, Gender gender)
+        {
+            List<Employee> filteredList = staff.Where(staff => staff.gender == gender).ToList();            
+            return filteredList;
+        }
+    }
+}
